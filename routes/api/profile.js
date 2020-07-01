64abe9b95ed41f2060cc10754e7e046ad522d466
Bradley Lewis <Blewie144@gmail.com>
2020-07-01 23:07:30 +0930

    Added delete for experiences and education and delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,7 +152,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
+    // remove users posts
+    await Post.deleteMany({ user: req.user });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
