5f33e04f3a0683e134a860d6074ef015a2e1512c
Bradley Lewis <Blewie144@gmail.com>
2020-07-05 16:38:16 +0930

    Added ad comment form and state

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -5,6 +5,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -56,6 +58,23 @@ export default function (state = initialState, action) {
           // Check if matches payload id if true, duplicate state but update it with the likes that are returned
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
+        loading: false,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+        },
         loading: false,
       };
     default:
